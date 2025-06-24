f6827cbcb23cce6e93e1bd4c77239211d1b1b5a6
kamau simon <kamausimon217@gmail.com>
2025-06-24 04:34:56 +0300

    fix: Remove unused QueryBuilder import in service_providers.rs

--- src/routes/service_providers.rs
+++ src/routes/service_providers.rs
@@ -11,7 +11,6 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::PgPool;
-use sqlx::QueryBuilder;
 use validator::Validate;
 
 pub fn service_providers_routes(pool: PgPool) -> Router {
